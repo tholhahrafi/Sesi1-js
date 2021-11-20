fee0af48f170b23c30697bce9ffc4b37583507f7
tholhahrafi <tholhahrafi22@gmail.com>
2021-11-20 16:43:47 +0700

    perubahan kedua

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 const nama = "tholhah rafi"; //const (konstanta) gabisa diubah
-let hobi = "main bola"; // let : bisa mengganti isisnya
-hobi = "nyanyi";
-console.log("Saya suka", hobi); // Ctrl+Shift+P (Format Document), "saya suka" (string)
+
+function getNama() {
+  return nama;
+}
+
+console.log(getNama()); // Ctrl+Shift+P (Format Document), "saya suka" (string)
