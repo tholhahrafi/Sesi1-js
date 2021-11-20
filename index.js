105c65ee808030f79c42a4b5b0b1b80d6977c4ac
tholhahrafi <tholhahrafi22@gmail.com>
2021-11-20 16:15:28 +0700

    testing pertama

---
+++ index.js
@@ -0,0 +1,4 @@
+const nama = "tholhah rafi"; //const (konstanta) gabisa diubah
+let hobi = "main bola"; // let : bisa mengganti isisnya
+hobi = "nyanyi";
+console.log("Saya suka", hobi); // Ctrl+Shift+P (Format Document), "saya suka" (string)
